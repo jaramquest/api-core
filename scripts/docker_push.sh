2dfa6747284c285ca875a45a17a611074525b967
Yongki Kim <cloudeyes@gmail.com>
2020-05-28 15:17:25 +0000

    Fix build script

--- scripts/docker_push.sh
+++ scripts/docker_push.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-echo '{"experimental":"enabled"}' | sudo tee $HOME/.docker/config.json
 docker build --squash -t jaramquest/api-core .
 docker push jaramquest/api-core
